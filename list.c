8951a6597cb5c54fa0a4be4230330cb9c53a0185
Replit user <replituser@example.com>
2021-03-24 00:34:53 +0000

    new try

--- list.c
+++ list.c
@@ -38,12 +38,12 @@ List * createList() {
 
 void * firstList(List * list) { 
     list->current = list->head;
-    return (void*)list->current->data;
+    return (void*)(list->current->data);
 }
 
 void * nextList(List * list) {
     list->current = list->current->next;
-    return (void*)list->current->data;
+    return (void*)(list->current->data);
 }
 
 void * lastList(List * list) {
