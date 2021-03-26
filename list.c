05e795400c1995fc5e6705460c51fe05ee9447ef
Replit user <replituser@example.com>
2021-03-26 23:48:33 +0000

    new try

--- list.c
+++ list.c
@@ -126,7 +126,7 @@ void * popBack(List * list) {
 
 void * popCurrent(List * list) {
   Node* aux = list->current;
-  Node* Sig = list->current->next;
+  //Node* Sig = list->current->next;
     if(list->tail==list->current){
       list->current=list->current->prev;
       //free(list->current->next);
@@ -134,7 +134,7 @@ void * popCurrent(List * list) {
       list->tail = list->current;
     }else if(list->head==list->current){
       list->current=list->current->next;
-     free(list->current->prev);
+      //free(list->current->prev);
       list->current->prev = NULL;
       list->head = list->current;
     }
@@ -142,7 +142,7 @@ void * popCurrent(List * list) {
       list->current->prev->next = list->current->next;
       list->current->next->prev = list->current->prev;
       //free(list->current);
-      list->current=Sig;
+    //  list->current=Sig;
     }
   
   return (void*)aux->data;
